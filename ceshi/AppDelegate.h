9336380bbe9c212967835c351b90543fb91badbb
Ios陈世誉 <shiyu.1007@foxmail.com>
2015-06-23 14:44:12 +0800

    Initial Commit

---
+++ ceshi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ceshi
+//
+//  Created by 陈世誉 on 15/6/23.
+//  Copyright (c) 2015年 hangyjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ceshi/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ceshi
+//
+//  Created by 陈世誉 on 15/6/23.
+//  Copyright (c) 2015年 hangyjx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
